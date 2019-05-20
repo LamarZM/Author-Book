14395b0ddf621abebda1043b5671ab681e630ee2
LamarZM <lamar.mufti@yahoo.com>
2019-05-20 14:47:27 +0300

    add relationship between author and book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,44 +1,48 @@
-class BooksController  < ApplicationController
+class BooksController < ApplicationController
     def index
-        @books = Book.all
+        @author = Author.find(params[:author_id])
+        @books = @author.books
+    end
+
+    def show 
+        @author = Author.find(params[:author_id])
+        @book = @author.books.find(params[:id])
+    end
+
+    def destroy
+        @author = Author.find(params[:author_id])
+        @book = @author.books.find(params[:id])
+        @book.destroy
+
+        redirect_to author_books_path(@author)
+    end
+
+    def new 
+        @author = Author.find(params[:author_id])
+        @book = @author.books.new
+    end
+
+    def create
+        @author = Author.find(params[:author_id])
+        @book = @author.books.create(book_params)
+        # redirect_to books_path
+        redirect_to author_book_path(@author, @book)
+    end
+
+    def edit 
+        @author = Author.find(params[:author_id])
+        @book = @author.books.find(params[:id])
+    end
+
+    def update 
+        @author = Author.find(params[:author_id])
+        @book = @author.books.find(params[:id])
+        @book.update(book_params)
+        # redirect_to books_path
+        redirect_to author_book_path(@author, @book)
+    end
+
+    def book_params
+        params.require(:book).permit(:title, :print_date, :original_language)
+    end
 end
-
-def show
-    @book = Book.find(params[:id])
-end
-
-def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-
-    redirect_to books_path
-end
-
-def new
-    @book = Book.new
-end
-
-def create
-    @Book = Book.create(book_params)
-    #redirect_to books_path
-    redirect_to books_path(@book)
-
-end
-
-def edit
-    @book = Book.find(params[:id])
-end
-
-def update
-    @book = Book.find(params[:id])
-    @book.update(book_params)
-    #redirect_to books_path
-    redirect_to book_path(@book)
-end
-
-def book_params
-    params.require(:book).permit(:title, :original_language, :print_date)
-end
-end
-
-
